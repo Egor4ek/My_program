69107facf76bd240650dafac86fb58ba5c31bc92
Egor <71322262+Egor4ek@users.noreply.github.com>
2021-03-08 22:14:43 +0300

    Add files via upload

---
+++ счёт букв.py
@@ -0,0 +1,8 @@
+s=input('Введите текст для подсчёта букв:\n')
+s=s.replace(' ','')
+n=len(s)
+if (n mod 2!=0):
+	print('в тексте '+str(n)+' буква.')
+
+if(n mod 5==0) or (n mod 3==0) or (n==7) or (n==8) or (n==11) or (n==13) or (n==14) or (n==17) or (n=19) or (n==26)
+	print('в тексте '+str(n)+' букв.')
